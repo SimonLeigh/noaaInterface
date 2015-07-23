5df834ec0527f1cfbc873d5c91d54aff80c395e6
Simon Leigh <simon.leigh@couchbase.com>
2015-07-23 13:14:07 +0100

    Updated views to be those required by latest app version,

--- ViewExample.js
+++ ViewExample.js
@@ -21,7 +21,7 @@ var byLatLon = function (doc, meta) {
  * byLatLonDate
  * Created by simon on 29/06/15.
  */
-var byLatLonDate = function (doc, meta) {
+function (doc, meta) {
 
     if (doc.LAT && doc.LON && doc.BEGIN && doc.END) {
 
@@ -32,7 +32,14 @@ var byLatLonDate = function (doc, meta) {
             /* Construct date by slicing out individual d/m/y */
             var begin = Date.UTC(doc.BEGIN.slice(0,4), doc.BEGIN.slice(4,6)-1, doc.BEGIN.slice(6,8))/1000
             var end = Date.UTC(doc.END.slice(0,4), doc.END.slice(4,6)-1, doc.END.slice(6,8))/1000
-            emit([longitude, latitude,begin,end], doc["STATION NAME"]);
+            emit([longitude, latitude,begin,end], {
+                    "Station Name": doc["STATION NAME"],
+                    "Longitude": doc["LON"],
+                    "Latitude": doc["LAT"],
+                    "Begin": doc["BEGIN"],
+                    "End": doc["END"]
+                }
+            );
         }
 
     }
